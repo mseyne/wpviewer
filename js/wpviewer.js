e3cd18a8068271d4c68d19556c0d2dda5f07346c
mseyne <michaelseyne@gmail.com>
2016-09-25 21:09:56 +0200

    add new cards

--- js/wpviewer.js
+++ js/wpviewer.js
@@ -14,7 +14,6 @@ function hideSearchBar(){
 }
 
 var getWikipediaResults = function(val){
-    $('#searchTitle').html(searchValue);
     $.ajax({
         url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + val + '&limit=' + numbResult + '&suggest=1&redirects=return&callback=ttt',
         jsonp: 'callback',
@@ -27,16 +26,27 @@ var getWikipediaResults = function(val){
 }
 
 var displayCards = function(){
-    console.log(resultJSON);
-    var i = 0;
-    while ( i < numbResult ){
-        for ( var result in resultJSON){
-            console.log(resultJSON[result][i]);
+    $('#results').html('');
+    $('#searchTitle').html(resultJSON[0]);
+    var article = [];
+    for (var i = 0; i < numbResult; i++){
+        for (var content of resultJSON){
+            if(content !== resultJSON[0]){
+                article.push(content[i]);
+            }
         }
-        i++
+        appendCard(article);
+        article = [];
     }
 }
 
+var appendCard = function(article){
+    var newCard = '<div class="divider"></div><a href="' + article[2] + '" target="_blank">';
+    newCard += '<article class="card-panel result-card hoverable amber lighten-4 z-depth-1">';
+    newCard += '<span class="title orange-text text-darken-4">' + article[0] + '</span> : '
+    newCard += '<span class="description grey-text text-darken-3">' + article[1] + '</span></article></a>'
+    $('#results').append(newCard);
+}
 
 $('document').ready(function(){
 
@@ -59,16 +69,22 @@ $('document').ready(function(){
         console.log('random');
     });
 
-    $('#search').bind("enterKey",function(e){
+    $('#search').keypress(function(event){
+        if (event.keyCode === 10 || event.keyCode === 13) 
+            event.preventDefault();
+    });
+
+    $('#search').bind("enterKeyPressed",function(e){
         searchValue = $('#search').val();
         hideSearchBar();
-        getWikipediaResults(searchValue);
+        if (searchValue.length > 0){
+            getWikipediaResults(searchValue);
+        }
     });
 
     $('#search').keyup(function(e){
-        if(e.keyCode == 13)
-        {
-            $(this).trigger("enterKey");
+        if(e.keyCode == 13){
+            $(this).trigger("enterKeyPressed");
         }
     });
 
